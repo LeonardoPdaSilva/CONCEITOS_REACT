291482f046cced1cd3337d63ca7f25ad2c0e5959
LEONARDO PEREIRA DA SILVA <leonardo_p_da-silva@estudante.sesisenai.org.br>
2026-04-29 09:11:59 -0300

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './paginas/login'
+import Cadastro from './paginas/cadastro'
+import Usuarios from './paginas/usuarios'
 
 function App() {
-  const [carro, setCarro] = useState('Tesla Model 3')
-function trocarDeCarro(){
-  setCarro('Porsche Taycan')
-  alert(carro)
-}
+  const [tela, setTela] = useState('Login')
+
+  const trocarDeTela = (pagina)=> {
+    setTela(pagina)
+  }
+
+  const renderizar = () => {
+    if(tela === 'Login'){
+      return <Login/>
+    } else if(tela === 'cadastro'){
+      return <Cadastro/>
+    } else if(tela === 'usuarios'){
+      return <Usuarios/>
+    }
+    else{
+      return <Login/>
+    }
+  }
+
   return (
     <>
-       <h1>{carro}</h1>
-    <button onClick={trocarDeCarro}>Trocar de Carro</button>
+    
+    <button onClick={() => trocarDeTela('cadastro')}>Cadastro</button>
+    <button onClick={() => trocarDeTela('login')}>Login</button>
+    <button onClick={() => trocarDeTela('usuarios')}>Usuários</button>
 
+    <hr/>
+    {renderizar()}
     </>
   )
 }
 
 export default App
+
