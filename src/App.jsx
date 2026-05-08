8b1f53ec32a236c12c3cdf74b854f4b36db84bd0
LEONARDO PEREIRA DA SILVA <leonardo_p_da-silva@estudante.sesisenai.org.br>
2026-05-08 09:12:52 -0300

    2° commit

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     {renderizar()}
     </>
   )
-}
+} 
 
 export default App
 
--- src/paginas/usuarios.jsx
+++ src/paginas/usuarios.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react"
+
 export default function Usuarios(){
-        
-        return(
+    const [cont, setCont] = useState(0)    
+
+    const aumenta = () => {
+        setCont(cont + 1)
+    }
+    const diminui = () => {
+        setCont(cont - 1)
+    }
+
+
+    useEffect(()=> {
+        document.title = `count: ${cont}`
+    }, [cont])
+
+    if(cont == 11){ 
+        alert("Passou de dez!")
+    }
+    if(cont == -10){
+        alert("Chegou a menos dez")
+    }
+
+
+    return(
+        <div>
             <h1>USUÁRIOS</h1>
+            {cont} <br />
+            <button button onClick={() => aumenta()}>Aumentar</button>
+             <br />
+            <button button onClick={() => diminui()}>Diminuir</button>
+        </div>
         )
+     
     }
